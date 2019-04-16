e91c593f775323f670a19d536eb5208cfc573e9c
Alex MacBook air <aleksandrmisnov@gmail.com>
2019-04-16 12:52:40 +0100

    Added a new branch for git pages

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const browserSync = require('browser-sync').create();
 // clean assets
 // Clean assets
 function clean() {
-    return del(["./dist/assets/", "!./dist/assets/img"]);
+    return del(["./docs/assets/", "!./docs/assets/img"]);
 }
 
 // compile sass and inject into browser
@@ -16,25 +16,25 @@ function clean() {
 function styles() {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
-        .pipe(gulp.dest('./dist/assets/css'))
+        .pipe(gulp.dest('./docs/assets/css'))
         .pipe(browserSync.stream());
 
 }
 
 function scripts() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(gulp.dest('docs/assets/js'))
         .pipe(browserSync.stream());
 }
 
 function watch() {
     browserSync.init({
         server: {
-            baseDir: './dist'
+            baseDir: './docs'
         }
     })
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], styles);
-    gulp.watch('./dist/*.html').on('change', browserSync.reload);
+    gulp.watch('./docs/*.html').on('change', browserSync.reload);
 
 }
 
@@ -42,12 +42,12 @@ function watch() {
 
 function fonts() {
     return gulp.src('node_modules/font-awesome/fonts/*')
-        .pipe(gulp.dest('./dist/assets/fonts'));
+        .pipe(gulp.dest('./docs/assets/fonts'));
 }
 
 function fa() {
     return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-        .pipe(gulp.dest('./dist/assets/css'));
+        .pipe(gulp.dest('./docs/assets/css'));
 }
 
 // set the default task
