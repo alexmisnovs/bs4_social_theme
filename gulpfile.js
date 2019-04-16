c47fc39b8aea67d3c039195e56bf8e3645336dbe
Alex MacBook air <aleksandrmisnov@gmail.com>
2019-04-16 17:55:36 +0100

    small changes to figure out why gitpages stopped working

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,69 @@
 "use strict";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 const del = require("del");
-const browserSync = require('browser-sync').create();
+const browserSync = require("browser-sync").create();
 
-// clean assets
-// Clean assets
+// delete css, js and fonts folders
 function clean() {
-    return del(["./docs/assets/", "!./docs/assets/img"]);
+  return del(["./docs/assets/css", "./docs/assets/js", "./docs/assets/fonts"]);
 }
 
 // compile sass and inject into browser
 
 function styles() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest('./docs/assets/css'))
-        .pipe(browserSync.stream());
-
+  return gulp
+    .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
+    .pipe(sass())
+    .pipe(gulp.dest("./docs/assets/css"))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest('docs/assets/js'))
-        .pipe(browserSync.stream());
+  return gulp
+    .src([
+      "node_modules/bootstrap/dist/js/bootstrap.min.js",
+      "node_modules/jquery/dist/jquery.min.js",
+      "node_modules/popper.js/dist/umd/popper.min.js"
+    ])
+    .pipe(gulp.dest("docs/assets/js"))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './docs'
-        }
-    })
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], styles);
-    gulp.watch('./docs/*.html').on('change', browserSync.reload);
-
+  browserSync.init({
+    server: {
+      baseDir: "./docs"
+    }
+  });
+  gulp.watch(
+    ["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"],
+    styles
+  );
+  gulp.watch("./docs/*.html").on("change", browserSync.reload);
 }
 
 // move font awesome to dist
 
 function fonts() {
-    return gulp.src('node_modules/font-awesome/fonts/*')
-        .pipe(gulp.dest('./docs/assets/fonts'));
+  return gulp
+    .src("node_modules/font-awesome/fonts/*")
+    .pipe(gulp.dest("./docs/assets/fonts"));
 }
 
 function fa() {
-    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-        .pipe(gulp.dest('./docs/assets/css'));
+  return gulp
+    .src("node_modules/font-awesome/css/font-awesome.min.css")
+    .pipe(gulp.dest("./docs/assets/css"));
 }
 
 // set the default task
-const build = gulp.series(clean, gulp.parallel(fa, fonts, styles, scripts), watch);
-
+const build = gulp.series(
+  clean,
+  gulp.parallel(fa, fonts, styles, scripts),
+  watch
+);
 
 exports.fonts = fonts;
 exports.clean = clean;
